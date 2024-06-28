9c58f9e09383bbf7d7f3aadfbff8fb7fc17e23ba
Mukund Vashisht <Mukundvashisht3@gmail.com>
2024-06-28 14:40:04 +0530

    Create Navbar Component

--- src/App.js
+++ src/App.js
@@ -15,42 +15,13 @@ export default class App extends Component {
         <Router>
           <Navbar />
           <Routes>
-            <Route
-              path="/"
-              element={<News key="general" country="us" category="General" />}
-            ></Route>
-            <Route
-              path="/business"
-              element={<News key="business" country="us" category="Business" />}
-            ></Route>
-            <Route
-              path="/entertainment"
-              element={
-                <News
-                  key="entertainment"
-                  country="us"
-                  category="Entertainment"
-                />
-              }
-            ></Route>
-            <Route
-              path="/health"
-              element={<News key="health" country="us" category="Health" />}
-            ></Route>
-            <Route
-              path="/science"
-              element={<News key="science" country="us" category="Science" />}
-            ></Route>
-            <Route
-              path="/sports"
-              element={<News key="sports" country="us" category="Sports" />}
-            ></Route>
-            <Route
-              path="/technology"
-              element={
-                <News key="technology" country="us" category="Technology" />
-              }
-            ></Route>
+            <Route path="/" element={<News key="general" country="us" category="General" />}></Route>
+            <Route path="/business" element={<News key="business" country="us" category="Business" />}></Route>
+            <Route path="/entertainment" element={<News key="entertainment" country="us" category="Entertainment" />}></Route>
+            <Route path="/health" element={<News key="health" country="us" category="Health" />}></Route>
+            <Route path="/science" element={<News key="science" country="us" category="Science" />}></Route>
+            <Route path="/sports" element={<News key="sports" country="us" category="Sports" />}></Route>
+            <Route path="/technology" element={<News key="technology" country="us" category="Technology" />}></Route>
           </Routes>
         </Router>
       </div>
