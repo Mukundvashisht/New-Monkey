d1ac07d9186fc389f88a1d8d7f205b5e573d79bd
Mukund Vashisht <Mukundvashisht3@gmail.com>
2024-07-02 21:31:36 +0530

    Commit 5
    Displayed data from api using fetch api insted of samply data & display dummy image and dummy text for null values in data from api

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import DummyImage from '../Assets/DummyImage.png'
 
 export default class NewsItem extends Component {
     render() {
@@ -7,12 +8,14 @@ export default class NewsItem extends Component {
 
         return (
             <div>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card" >
+                    {/* <img src={imageUrl ? imageUrl : 'https://dummyimage.com/600x400/fcfcfc/baa2ba.png'} className="card-img-top" alt="..." style={{ height: "187px" }} /> */}
+                    <img src={imageUrl ? imageUrl : DummyImage} className="card-img-top" alt="..." style={{ height: "187px" }} />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsUrl} target='_blank' rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                        {/* <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a> */}
                     </div>
                 </div>
             </div>
