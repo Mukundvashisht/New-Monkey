1bd5afed8dfb15fc6c5b0fc581ca77efd8c52229
Mukund Vashisht <Mukundvashisht3@gmail.com>
2024-07-30 21:07:47 +0530

    commit 10
    used react-touter-dom & remount same news component for different categories accordingly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
   // static propTypes = { second: third }
@@ -11,8 +16,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News country="in" category="science" />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<News key="general" country="in" category="general" />}></Route>
+            <Route path="/business" element={<News key="business" country="in" category="business" />}></Route>
+            <Route path="/entertainment" element={<News key="entertainment" country="in" category="entertainment" />}></Route>
+            <Route path="/health" element={<News key="health" country="in" category="health" />}></Route>
+            <Route path="/science" element={<News key="science" country="in" category="science" />}></Route>
+            <Route path="/sports" element={<News key="sports" country="in" category="sports" />}></Route>
+            <Route path="/technology" element={<News key="technology" country="in" category="technology" />}></Route>
+          </Routes>
+
+        </Router>
       </div>
     )
   }
