df4d3363dec061fae5d91a13e201e2dc5dbaf3ed
Mukund Vashisht <Mukundvashisht3@gmail.com>
2024-07-30 12:22:53 +0530

    commit 8
    add a spinner

--- src/Components/spinner.js
+++ src/Components/spinner.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import Loader from './Loader.gif'
 
 export default class spinner extends Component {
-    render() {
-        return (
-            <div className='text-center my-3'>
-                <div className="spinner-grow" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="text-center my-3">
+        <div className="spinner-grow" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 }
